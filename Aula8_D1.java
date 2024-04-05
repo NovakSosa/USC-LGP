def7ae363b8e971b3fa96927a15d595387b76133
NovakSosa <164577639+NovakSosa@users.noreply.github.com>
2024-04-04 23:34:29 -0300

    Create Aula8_D1.java

---
+++ Aula8_D1.java
@@ -0,0 +1,33 @@
+package Aula8;
+
+import java.util.Scanner;
+
+public class Desafio1 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Digite a idade do paciente: ");
+		int idade = scan.nextInt();
+		System.out.println("Digite o batimento cardiaco do paciente: ");
+		int bc = scan.nextInt();
+		String classificacao = "";
+		
+		if (idade > 60 && bc > 150) {
+			classificacao = "Urgente";
+		}
+		else if (idade > 60 && bc <= 150) {
+			classificacao = "Urgência moderada";
+		}
+		else if (idade <= 60 && bc > 150) {
+			classificacao = "Urgência moderada";
+		}
+		else if (idade <= 60 && bc <= 150) {
+			classificacao = "Não urgente";
+		}
+		
+		classificacao = (classificacao.equals("Urgente")) ? classificacao.toUpperCase() : classificacao;
+		System.out.println("Classificação do paciente: " + classificacao);
+		scan.close();
+	}
+
+}
